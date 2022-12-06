280cdac2d470298501ffdb907956fad03c609540
trigger737 <Papirnicka14>
2022-12-06 08:44:17 +0100

    Added day 6.

---
+++ Day 6/first.py
@@ -0,0 +1,15 @@
+with open('input.txt', 'r') as file:
+    data = file.read().strip()
+
+buff = []
+result = ''
+str_len = 4
+for letter in data:
+    buff.append(letter)
+    if len(buff) >= str_len:
+        pack = list(dict.fromkeys(buff))
+        if len(pack) == str_len:
+            result = ''.join(pack)
+            break
+        buff.pop(0)
+print(result, data.find(result) + str_len)
